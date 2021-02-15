34860985ae47345b33e2c303802bfc25806bbbee
gnd <gnd@itchybit.org>
2021-02-15 23:18:59 +0100

    first commit woo

---
+++ db.py
@@ -0,0 +1,34 @@
+import MySQLdb
+
+class db_class():
+    def __init__(self, db_host, db_user, db_pass, db_name):
+        self.db_host = db_host
+        self.db_user = db_user
+        self.db_pass = db_pass
+        self.db_name = db_name
+
+    def open_db(self):
+        self.db = MySQLdb.connect(host=self.db_host, user=self.db_user, passwd=self.db_pass, db=self.db_name)
+        self.cur = self.db.cursor()
+
+    def close_db(self):
+        self.db.close()
+
+    def insert_pair_data(self, pair_name, pair_data):
+        query = "INSERT INTO {} VALUES(0, '{}', {}, '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', NOW())".format(
+        'basic_ticker',
+        pair_name, 
+        pair_data['id'], 
+        pair_data['last'], 
+        pair_data['last_position'], 
+        pair_data['lowestAsk'],
+        pair_data['highestBid'],
+        pair_data['percentChange'],
+        pair_data['baseVolume'],
+        pair_data['quoteVolume'],
+        pair_data['isFrozen'],
+        pair_data['high24hr'],
+        pair_data['low24hr']
+        )
+        self.cur.execute(query)
+        self.db.commit()
---
+++ polobot.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from db import db_class
+import urllib.request
+import configparser
+import json
+
+### load config
+config = configparser.ConfigParser()
+config.read_file(open('settings_python'))
+DB_HOST = config.get('database', 'DB_HOST')
+DB_USER = config.get('database', 'DB_USER')
+DB_PASS = config.get('database', 'DB_PASS')
+DB_NAME = config.get('database', 'DB_NAME')
+API_URL = config.get('ticker', 'API_URL')
+API_PAIR = config.get('ticker', 'API_PAIR')
+
+def getResponse(url):
+    operUrl = urllib.request.urlopen(url)
+    if (operUrl.getcode()==200):
+        data = operUrl.read()
+        jsonData = json.loads(data)
+    else:
+        print("Error receiving data", operUrl.getcode())
+    return jsonData
+
+def main():
+    data = getResponse(API_URL)
+    pair_data = data[API_PAIR]
+    daily_range = float(pair_data['high24hr']) - float(pair_data['low24hr'])
+    pair_data['last_position'] = (float(pair_data['last']) - float(pair_data['low24hr'])) / daily_range
+    
+    # insert pair_data into db
+    db = db_class(DB_HOST, DB_USER, DB_PASS, DB_NAME)
+    db.open_db()
+    db.insert_pair_data(API_PAIR, pair_data)
+    db.close_db()
+
+if __name__ == '__main__':
+    main()
